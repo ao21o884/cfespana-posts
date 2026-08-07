4049fa154df6515002b73a0833f0550b65e39e9b
ao21o884 <124585831+ao21o884@users.noreply.github.com>
2026-08-07 17:50:04 +0200

    Update publish.py

--- publish.py
+++ publish.py
@@ -70,6 +70,7 @@ def post_buffer(png, caption):
           channelId: """ + json_str(channel_id) + """
           schedulingType: automatic
           mode: addToQueue
+          instagramOptions: { type: post }
           assets: [
             {
               image: {
